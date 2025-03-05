41f39545e1d06a73b896ec4c9bf862c377a4d293
urabexon <hiroki708.a48uk@gmail.com>
2025-03-05 20:48:58 +0900

    add ex00

--- ex00/includes/Bureaucrat.hpp
+++ ex00/includes/Bureaucrat.hpp
@@ -6,7 +6,43 @@
 /*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:26:13 by hirokiurabe       #+#    #+#             */
-/*   Updated: 2025/03/05 18:26:14 by hirokiurabe      ###   ########.fr       */
+/*   Updated: 2025/03/05 20:47:29 by hirokiurabe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef Bureaucrat_hpp
+#define Bureaucrat_hpp
+
+#include <iostream>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
+
+class Bureaucrat {
+    public:
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        ~Bureaucrat();
+        Bureaucrat &operator=(const Bureaucrat &copy);
+
+        const std::string &getName() const;
+        int     getGrade() const;
+        void    incrementGrade();
+        void    decrementGrade();
+
+        class GradeTooLowException : public std::exception {
+            const char *what() const throw();
+        };
+
+        class GradeTooHighException : public std::exception {
+            const char *what() const throw();
+        };
+
+    private:
+        const std::string _name;
+        int _grade;
+};
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &bureaucrat);
+
+#endif
--- ex00/srcs/Bureaucrat.cpp
+++ ex00/srcs/Bureaucrat.cpp
@@ -6,7 +6,60 @@
 /*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:26:16 by hirokiurabe       #+#    #+#             */
-/*   Updated: 2025/03/05 18:26:17 by hirokiurabe      ###   ########.fr       */
+/*   Updated: 2025/03/05 20:47:07 by hirokiurabe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade) {
+    if (grade > MIN_GRADE)
+        throw GradeTooLowException();
+    else if (grade < MAX_GRADE)
+        throw GradeTooHighException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) {
+    *this = copy;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy) {
+    if (this != &copy)
+        this->_grade = copy._grade;
+    return *this;
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Grade is too low.";
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return "GRADE is too high.";
+}
+
+const std::string &Bureaucrat::getName() const {
+    return this->_name;
+}
+
+int Bureaucrat::getGrade() const {
+    return this->_grade;
+}
+
+void    Bureaucrat::incrementGrade() {
+    if (_grade - 1 < MAX_GRADE)
+        throw GradeTooHighException();
+    _grade -= 1;
+}
+
+void    Bureaucrat::decrementGrade() {
+    if (_grade + 1 > MIN_GRADE)
+        throw GradeTooLowException();
+    _grade += 1;
+}
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &bureaucrat) {
+    o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "." << std::endl;
+    return o;
+}
