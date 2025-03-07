cd035d975ca63e35feb9f2022920ea412591edb3
urabexon <hiroki708.a48uk@gmail.com>
2025-03-07 14:17:59 +0900

    test push

--- ex01/includes/Bureaucrat.hpp
+++ ex01/includes/Bureaucrat.hpp
@@ -3,10 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:29:23 by hirokiurabe       #+#    #+#             */
-/*   Updated: 2025/03/05 18:29:25 by hirokiurabe      ###   ########.fr       */
+/*   Updated: 2025/03/07 14:16:13 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+test
