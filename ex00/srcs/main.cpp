2b57132d1f24dbf32c0fdf4932e729fc9fd5af73
urabexon <hiroki708.a48uk@gmail.com>
2025-03-07 14:38:41 +0900

    major bug all clean

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -3,28 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:26:19 by hirokiurabe       #+#    #+#             */
-/*   Updated: 2025/03/05 20:23:31 by hirokiurabe      ###   ########.fr       */
+/*   Updated: 2025/03/07 14:37:00 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
-    try {
-        Bureaucrat bob("Bob", 1);
-        std::cout << bob;
-    } catch (std::exception& e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
-
-    try {
-        Bureaucrat hima("hima", 150);
-        std::cout << hima;
-    } catch (std::exception& e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
     return 0;
 }
